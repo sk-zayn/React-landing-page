4073cbff1010d1f9e689dbf3d7f949cd625d4913
Zain Shaikh <skzain007995@gmail.com>
2026-06-20 22:16:08 +0530

    Add Navbar component

---
+++ src/components/Section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-10 px-14'>
+      <h4 className='bg-black text-white text-sm tracking-widest px-5 py-2 rounded-full uppercase'>Target Audience</h4>
+      <button className='px-5 py-3 tracking-widest text-sm uppercase'>Digital Banking Platform</button>
+    </div>
+  )
+}
+
+export default Navbar
